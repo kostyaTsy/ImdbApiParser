80638fdc5880039a1e0b8b57824784242507a968
Kostya <tsyvilko12@gmail.com>
2022-05-24 10:58:32 +0300

    Initial Commit

---
+++ ImdbApiParser/ImdbApiParserApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImdbApiParserApp.swift
+//  ImdbApiParser
+//
+//  Created by Kostya Tsyvilko on 24.05.22.
+//
+
+import SwiftUI
+
+@main
+struct ImdbApiParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
